01ce742bed7fe30f18ed34deb7e7dd9cd4bb3eca
rcehemann <31669884+rcehemann@users.noreply.github.com>
2017-12-04 22:45:42 -0500

    added test.py

---
+++ python_sentiment/setup_py.sh
@@ -0,0 +1,8 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-4.3.30-Linux-x86_64.sh
+bash Miniconda3-4.3.30-Linux-x86_64.sh
+/home/thekeele/miniconda3/bin/conda install -y numpy
+/home/thekeele/miniconda3/bin/conda install -y scipy
+/home/thekeele/miniconda3/bin/conda install -y pandas
+/home/thekeele/miniconda3/bin/conda install -y scikit-learn
+#/home/thekeele/miniconda3/bin/conda install nltk-with-data
+rm -f Miniconda3-4.3.30-Linux-x86_64.sh
