8e22abac6ca5ee44ee5c024195b279721f85e333
rcehemann <31669884+rcehemann@users.noreply.github.com>
2017-12-04 22:16:52 -0500

    updated setup script

--- setup_py.sh
+++ setup_py.sh
@@ -1,7 +1,7 @@
-wget http://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
-bash Anaconda3-4.3.0-Linux-x86_64.sh
-conda install numpy
-conda install scipy
-conda install pandas
-conda install scikit-learn
-conda install nltk-with-data
+wget https://repo.continuum.io/miniconda/Miniconda3-4.3.30-Linux-x86_64.sh
+bash Miniconda3-4.3.30-Linux-x86_64.sh
+/home/thekeele/miniconda3/bin/conda install numpy
+/home/thekeele/miniconda3/bin/conda install scipy
+/home/thekeele/miniconda3/bin/conda install pandas
+/home/thekeele/miniconda3/bin/conda install scikit-learn
+#conda install nltk-with-data
