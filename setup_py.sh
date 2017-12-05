2331763d5d9540081a8710764493d62d1e0b188c
rcehemann <31669884+rcehemann@users.noreply.github.com>
2017-12-04 21:48:12 -0500

    added python installation script

---
+++ setup_py.sh
@@ -0,0 +1,7 @@
+wget http://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh
+bash Anaconda3-4.3.0-Linux-x86_64.sh
+conda install numpy
+conda install scipy
+conda install pandas
+conda install scikit-learn
+conda install nltk-with-data
