8ae0c9953f4dc31b79ee79bc1166265bea11f314
Mark Keele <thekeele@dev.keele.me>
2017-12-04 22:57:50 -0500

    remove file

--- setup_py.sh
+++
@@ -1,7 +0,0 @@
-wget https://repo.continuum.io/miniconda/Miniconda3-4.3.30-Linux-x86_64.sh
-bash Miniconda3-4.3.30-Linux-x86_64.sh
-/home/thekeele/miniconda3/bin/conda install numpy
-/home/thekeele/miniconda3/bin/conda install scipy
-/home/thekeele/miniconda3/bin/conda install pandas
-/home/thekeele/miniconda3/bin/conda install scikit-learn
-#conda install nltk-with-data
