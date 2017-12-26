fb0a08162ca4b63164d0ca5af20f70b2ef50136c
Mark Keele <mark@onxmaps.com>
2017-12-25 20:26:12 -0600

    endpoint config

--- ex_feels/config/prod.exs
+++ ex_feels/config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :ex_feels, ExFeelsWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "10.132.213.230", port: 9006],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
