37230de4051faf4953f5c378b0a1e19933d1e382
Mark Keele <mark@onxmaps.com>
2017-12-30 18:24:59 -0600

    display feels on the home page (#13)

--- ex_feels/lib/ex_feels_web/controllers/home_controller.ex
+++ ex_feels/lib/ex_feels_web/controllers/home_controller.ex
@@ -1,7 +1,11 @@
 defmodule ExFeelsWeb.HomeController do
   use ExFeelsWeb, :controller
 
+  alias ExFeels.Feel
+
   def home(conn, _params) do
-    render conn, "home.html"
+    feels = Feel.all()
+
+    render(conn, "home.html", feels: feels)
   end
 end
--- ex_feels/lib/ex_feels_web/views/home_view.ex
+++ ex_feels/lib/ex_feels_web/views/home_view.ex
@@ -1,3 +1,13 @@
 defmodule ExFeelsWeb.HomeView do
   use ExFeelsWeb, :view
+
+  def binary_sentiment(sentiment) when is_binary(sentiment) do
+    sentiment = String.to_integer(sentiment)
+
+    cond do
+      sentiment == -1 -> "😭"
+      sentiment == 0 -> "😐"
+      sentiment == 1 -> "🤑"
+    end
+  end
 end
