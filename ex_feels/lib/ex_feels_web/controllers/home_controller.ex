d2ff4772e389b3c63521c8e2764b9ff23f39d197
Chris <rcehemann@gmail.com>
2018-03-04 19:20:31 -0500

    display number of stats

--- ex_feels/lib/ex_feels_web/controllers/home_controller.ex
+++ ex_feels/lib/ex_feels_web/controllers/home_controller.ex
@@ -1,15 +1,17 @@
 defmodule ExFeelsWeb.HomeController do
   use ExFeelsWeb, :controller
 
-  alias ExFeels.{Feel, Twitter.Tweet, Twitter.User}
+  alias ExFeels.{Feel, Stat, Twitter.Tweet, Twitter.User}
 
-  def home(conn, _params) do
-    feels = group_by_tweet(Feel.all())
+  def home(conn, params) do
     counts = %{
         :feels => Feel.count(),
         :tweets => Tweet.count(),
-        :users => User.count()
+        :users => User.count(),
+        :stats => Stat.count()
     }
+
+    current_page = params["page"] || 1
 
     page = Feel.all(page: current_page, page_size: 24)
 
