2c191f86502f95787d951933e0bcfbe9fc04ce6c
Mark Keele <mark@onxmaps.com>
2018-03-09 19:28:59 -0700

    delete stats then remove/add time column, up/down for rollbacks

--- ex_feels/priv/repo/migrations/20180310013957_alter_stats_modify_time_integer.exs
+++ ex_feels/priv/repo/migrations/20180310013957_alter_stats_modify_time_integer.exs
@@ -1,9 +1,23 @@
 defmodule ExFeels.Repo.Migrations.AlterStatsModifyTimeInteger do
   use Ecto.Migration
 
-  def change do
+  def up do
+    execute("DELETE FROM stats;")
+
     alter table("stats") do
-      modify :time, :integer
+      remove :time
+
+      add :time, :integer
+    end
+  end
+
+  def down do
+    execute("DELETE FROM stats;")
+
+    alter table("stats") do
+      remove :time
+
+      add :time, :string
     end
   end
 end
