1e373398a0368028bb38396d1c8243f76287b105
Mark Keele <mark@onxmaps.com>
2018-03-18 12:00:25 -0600

    bump version

--- ex_feels/mix.exs
+++ ex_feels/mix.exs
@@ -4,7 +4,7 @@ defmodule ExFeels.Mixfile do
   def project do
     [
       app: :ex_feels,
-      version: "1.2.0",
+      version: "1.2.1",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
