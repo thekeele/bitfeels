0cab509dca65c3c078aa6f3bde4ef1b8d11ba3e4
Mark Keele <mark@onxmaps.com>
2018-05-18 12:59:45 -0600

    add pyyaml to the server install script

--- py_feels/setup_py.sh
+++ py_feels/setup_py.sh
@@ -7,5 +7,6 @@ bash Miniconda3-4.3.30-Linux-x86_64.sh
 /home/thekeele/miniconda3/bin/conda install -y psutil
 /home/thekeele/miniconda3/bin/conda install -y psycopg2
 /home/thekeele/miniconda3/bin/conda install -y sqlalchemy
+/home/thekeele/miniconda3/bin/conda install -y pyyaml
 rm -f Miniconda3-4.3.30-Linux-x86_64.sh
 /home/thekeele/miniconda3/bin/python test.py
