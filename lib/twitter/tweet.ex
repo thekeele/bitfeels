597b2b742e61e61f6b43aca9f55a13c332649b0d
Mark Keele <mark@keele.codes>
2018-12-06 20:20:53 -0700

    refactor tweet parsing

--- lib/twitter/tweet.ex
+++ lib/twitter/tweet.ex
@@ -24,25 +24,37 @@ defmodule Bitfeels.Tweet do
   # end
 
   def parse_to_tweet(status) when is_map(status) do
-    status = status["retweeted_status"] || status
-
     status
-    |> take_status_data()
-    |> Map.put("hashtags", take_hashtags_data(status["entities"]["hashtags"]))
-    |> Map.put("user", take_user_data(status["user"]))
+    |> Map.take(["created_at", "id","retweet_count", "favorite_count", "lang"])
+    |> Map.put("text", parse_tweet_text(status))
+    |> Map.put("hashtags", parse_hashtags(status))
+    |> Map.put("user", parse_user(status))
   end
 
-  defp take_status_data(status) do
-    keys = ["created_at", "id", "text", "full_text", "retweet_count", "favorite_count", "lang"]
+  defp parse_tweet_text(%{"extended_tweet" => %{"full_text" => text}}),
+    do: text
 
-    Map.take(status, keys)
-  end
+  defp parse_tweet_text(%{"retweeted_status" => status}),
+    do: status["extended_tweet"]["full_text"] || status["text"]
 
-  defp take_hashtags_data(hashtags), do: Enum.map(hashtags, & &1["text"])
+  defp parse_tweet_text(%{"quoted_status" => status}),
+    do: status["extended_tweet"]["full_text"] || status["text"]
 
-  defp take_user_data(user) do
+  defp parse_tweet_text(%{"text" => text}),
+    do: text
+
+  defp parse_hashtags(%{"entities" => %{"hashtags" => hashtags}}),
+    do: Enum.map(hashtags, & &1["text"])
+
+  defp parse_user(%{"user" => user}) do
     keys = [
-      "id", "screen_name", "followers_count", "favourites_count", "time_zone", "verified", "statuses_count"
+      "id",
+      "screen_name",
+      "followers_count",
+      "favourites_count",
+      "time_zone",
+      "verified",
+      "statuses_count"
     ]
 
     Map.take(user, keys)
