45b7ad3bb91c0a8d89afb59593a63a5d0fe0b705
Mark Keele <mark@keele.codes>
2018-12-06 20:05:44 -0700

    allow application children configs to be overwritable

--- lib/application.ex
+++ lib/application.ex
@@ -4,12 +4,20 @@ defmodule Bitfeels.Application do
   def start(_type, _args) do
     import Supervisor.Spec
 
-    twitter_stream = Application.get_env(:bitfeels, :twitter_stream, %{"track" => "bitcoin"})
+    twitter_stream = Application.get_env(:bitfeels, :twitter_stream)
+    tweet_pipeline = Application.get_env(:bitfeels, :tweet_pipeline)
 
     children = [
-      worker(TwitterStream.RealtimeTweets, [twitter_stream]),
+      worker(TwitterStream.RealtimeTweets, [%{
+        "track" => twitter_stream[:track] || "bitcoin",
+        "language" => twitter_stream[:language] || "en",
+        "filter_level" => twitter_stream[:filter_level] || "none"
+      }]),
       worker(Bitfeels.TweetSource, []),
-      supervisor(Bitfeels.TweetPipeline, [[max_demand: 10, min_demand: 3]]),
+      supervisor(Bitfeels.TweetPipeline, [[
+        max_demand: tweet_pipeline[:max_demand] || 10,
+        min_demand: tweet_pipeline[:min_demand] || 3
+      ]]),
     ]
 
     opts = [strategy: :one_for_one, name: Bitfeels.Supervisor]
