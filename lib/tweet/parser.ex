9a53e805ab387466c75fd1e1fff21f45053146fb
Mark Keele <mark@keele.codes>
2019-06-04 13:51:37 -0600

    parse inner tweet for user data, add more user keys

--- lib/tweet/parser.ex
+++ lib/tweet/parser.ex
@@ -1,24 +1,32 @@
 defmodule Bitfeels.Tweet.Parser do
 
-  def parse_to_tweet(status) when is_map(status) do
+  def parse_to_tweet(%{"extended_tweet" => %{"full_text" => text}} = status) do
+    parse_tweet_data(status, text)
+  end
+
+  def parse_to_tweet(%{"retweeted_status" => status}) do
+    text = status["extended_tweet"]["full_text"] || status["text"]
+
+    parse_tweet_data(status, text)
+  end
+
+  def parse_to_tweet(%{"quoted_status" => status}) do
+    text = status["extended_tweet"]["full_text"] || status["text"]
+
+    parse_tweet_data(status, text)
+  end
+
+  def parse_to_tweet(%{"text" => text} = status) do
+    parse_tweet_data(status, text)
+  end
+
+  defp parse_tweet_data(status, text) do
     status
-    |> Map.take(["created_at", "id","retweet_count", "favorite_count", "lang"])
-    |> Map.put("text", parse_tweet_text(status))
+    |> Map.take(["created_at", "id", "reply_count", "retweet_count", "favorite_count", "lang"])
+    |> Map.put("text", text)
     |> Map.put("hashtags", parse_hashtags(status))
     |> Map.put("user", parse_user(status))
   end
-
-  defp parse_tweet_text(%{"extended_tweet" => %{"full_text" => text}}),
-    do: text
-
-  defp parse_tweet_text(%{"retweeted_status" => status}),
-    do: status["extended_tweet"]["full_text"] || status["text"]
-
-  defp parse_tweet_text(%{"quoted_status" => status}),
-    do: status["extended_tweet"]["full_text"] || status["text"]
-
-  defp parse_tweet_text(%{"text" => text}),
-    do: text
 
   defp parse_hashtags(%{"entities" => %{"hashtags" => hashtags}}),
     do: Enum.map(hashtags, & &1["text"])
@@ -26,12 +34,15 @@ defmodule Bitfeels.Tweet.Parser do
   defp parse_user(%{"user" => user}) do
     keys = [
       "id",
+      "name",
+      "verified",
       "screen_name",
+      "profile_image_url",
+      "statuses_count",
       "followers_count",
       "favourites_count",
-      "time_zone",
-      "verified",
-      "statuses_count"
+      "location",
+      "time_zone"
     ]
 
     Map.take(user, keys)
