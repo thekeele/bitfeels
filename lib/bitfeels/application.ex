7fda8af1419438a8f36d3f8fedab10a087f8c4e9
Mark Keele <mark@keele.codes>
2019-07-21 12:54:49 -0600

    put key name back

--- lib/bitfeels/application.ex
+++ lib/bitfeels/application.ex
@@ -2,7 +2,7 @@ defmodule Bitfeels.Application do
   use Application
 
   def start(_type, _args) do
-    sink = Application.get_env(:bitfeels, :stream)[:sink] || self()
+    sink = Application.get_env(:bitfeels, :twitter_stream)[:sink] || self()
 
     children = [
       {Registry, keys: :unique, name: Registry.Streams},
