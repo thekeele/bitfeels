54f2c95b947f20e47432633318ddd49214e7d79b
Mark Keele <mark@keele.codes>
2019-07-29 19:14:17 -0600

    Dynamic Stream Supervisor (#47)
    * adds dynamic stream api, supervisor for streams, rename data pipeline
    
    * bump deps version
    
    * put key name back
    
    * bump twitter_stream dep
    
    * requires elixir 1.9

--- lib/bitfeels/pipeline/sentiment.ex
+++ lib/bitfeels/pipeline/sentiment.ex
@@ -1,24 +1,24 @@
-defmodule Bitfeels.TweetFeels do
+defmodule Bitfeels.Pipeline.Sentiment do
   use GenStage
 
-  alias Bitfeels.Tweet
+  alias Bitfeels.Tweet.{Parser, Sentiment}
 
   def start_link(opts) do
     GenStage.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
   def init(opts) do
-    {:consumer, opts, subscribe_to: [{Bitfeels.TweetDispatcher, opts}]}
+    {:consumer, opts, subscribe_to: [{Bitfeels.Pipeline.Dispatcher, opts}]}
   end
 
   def handle_events(tweets, _from, opts) do
     for %{"id" => _} = status <- tweets do
-      tweet = Tweet.Parser.parse_to_tweet(status)
+      tweet = Parser.parse_to_tweet(status)
 
       tweet_with_sentiment =
         tweet
-        |> Tweet.Sentiment.sentiment_analysis()
-        |> Tweet.Sentiment.put_sentiment_score(tweet)
+        |> Sentiment.sentiment_analysis()
+        |> Sentiment.put_sentiment_score(tweet)
 
       send(opts[:sink], {:tweet, {tweet["id"], tweet_with_sentiment}})
     end
